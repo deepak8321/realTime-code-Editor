e5aa0d29c827dc6fd1e07894af5783f6b444bffa
deepak8321 <deepakpaswan8321@gmail.com>
2023-07-16 07:29:49 +0530

    initial

--- src/socket.js
+++ src/socket.js
@@ -7,6 +7,6 @@ export const initSocket = async () => {
         timeout: 10000,
         transports: ['websocket'],
     };
-    return io('http://localhost:5000', options);
+    return io(process.env.RECT_APP_BACKEND_URL, options);
 };
 // process.env.RECT_APP_BACKEND_URL http://localhost:5000
